3019c1b3b3eb15a098c0ab2d23840df73111b175
HN721 <hosea1422@gmail.com>
2025-07-01 18:00:58 +0700

    feat: create user controller

--- controller/Users.go
+++ controller/Users.go
@@ -1,7 +1,32 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"weeklytickits/models"
+	"weeklytickits/utils"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetUser(ctx *gin.Context) {
-
+	var input models.Users
+	err := ctx.ShouldBind(&input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, utils.Response{
+			Success: false,
+			Message: "Invalid Input",
+		})
+	}
+	result, err := models.FindAllUser()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, utils.Response{
+			Success: false,
+			Message: "Something Wrong On Database",
+		})
+	}
+	ctx.JSON(http.StatusOK, utils.Response{
+		Success: true,
+		Message: "OK",
+		Results: result,
+	})
 }
