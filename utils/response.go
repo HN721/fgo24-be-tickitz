ad145991ade7c194ead0f32575798ebb786ecd5a
HN721 <hosea1422@gmail.com>
2025-07-01 17:43:15 +0700

    feat: create Response model

---
+++ utils/response.go
@@ -0,0 +1,7 @@
+package utils
+
+type Response struct {
+	Success bool   `json:"success" form:"sucess"`
+	Message string `json:"message" form:"message"`
+	Results any    `json:"results" form:"results"`
+}
