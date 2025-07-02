73a08f8515e831ca76ccc58f1e903235293279d1
HN721 <hosea1422@gmail.com>
2025-07-02 23:28:18 +0700

    feat: create getAllMovies to show all movie from database

--- models/movie.go
+++ models/movie.go
@@ -1,1 +1,30 @@
 package models
+
+import (
+	"context"
+	"weeklytickits/utils"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type movies struct {
+	title        string
+	synopsis     string
+	background   string
+	poster       string
+	release_date string
+	duration     int
+	price        int
+}
+
+func GetAllMovies() []movies {
+	conn, err := utils.DBConnect()
+	if err != nil {
+
+	}
+	query := `SELECT id,title,synopsis,background,poster,release_date,duration,price FROM movies`
+	result, err := conn.Query(context.Background(), query)
+	data, err := pgx.CollectRows[movies](result, pgx.RowToStructByName)
+	return data
+
+}
