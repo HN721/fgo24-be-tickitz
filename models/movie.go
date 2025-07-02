0997b730556ad33564344476622418fd5b242ad3
HN721 <hosea1422@gmail.com>
2025-07-02 23:59:59 +0700

    feat:create func insert movie in model

--- models/movie.go
+++ models/movie.go
@@ -2,29 +2,45 @@ package models
 
 import (
 	"context"
+	"time"
 	"weeklytickits/utils"
 
 	"github.com/jackc/pgx/v5"
 )
 
-type movies struct {
-	title        string
-	synopsis     string
-	background   string
-	poster       string
-	release_date string
-	duration     int
-	price        int
+type Movies struct {
+	Title       string    `db:"title"`
+	Synopsis    string    `db:"synopsis"`
+	Background  string    `db:"background"`
+	Poster      string    `db:"poster"`
+	ReleaseDate time.Time `db:"release_date"`
+	Duration    int       `db:"duration"`
+	Price       int       `db:"price"`
 }
 
-func GetAllMovies() []movies {
+func GetAllMovies() ([]Movies, error) {
 	conn, err := utils.DBConnect()
 	if err != nil {
 
 	}
-	query := `SELECT id,title,synopsis,background,poster,release_date,duration,price FROM movies`
+	query := `SELECT title, synopsis, background, poster, release_date, duration, price FROM movies`
 	result, err := conn.Query(context.Background(), query)
-	data, err := pgx.CollectRows[movies](result, pgx.RowToStructByName)
-	return data
+	data, err := pgx.CollectRows[Movies](result, pgx.RowToStructByName)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 
+}
+func InsertMovies(movie Movies) error {
+	conn, err := utils.DBConnect()
+	if err != nil {
+		return err
+	}
+	query := `INSERT INTO movies(title, synopsis, background, poster, release_date, duration, price)VALUES($1,$2,$3,$4,$5,$6,$7)`
+	_, err = conn.Exec(context.Background(), query, movie.Title, movie.Synopsis, movie.Background, movie.Poster, movie.ReleaseDate, movie.Duration, movie.Price)
+	defer func() {
+		conn.Conn().Close(context.Background())
+	}()
+	return err
 }
