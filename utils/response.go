27da0d6c78c2df497f74710bbadb2fe834d3091a
HN721 <hosea1422@gmail.com>
2025-07-02 16:40:57 +0700

    feat: add response  error on struct

--- utils/response.go
+++ utils/response.go
@@ -4,4 +4,5 @@ type Response struct {
 	Success bool   `json:"success" form:"sucess"`
 	Message string `json:"message" form:"message"`
 	Results any    `json:"results" form:"results"`
+	Error   string `json:"error"`
 }
