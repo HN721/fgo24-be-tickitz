9eed7faf75622531f1edc88f1727421d96b93fca
HN721 <hosea1422@gmail.com>
2025-07-02 11:55:14 +0700

    feat: create router for user login,register and change password

--- routers/usersRoute.go
+++ routers/usersRoute.go
@@ -1,3 +1,13 @@
 package routers
 
-func authRoute()
+import (
+	"weeklytickits/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func authRouter(r *gin.RouterGroup) {
+	r.POST("/register", controller.Register)
+	r.POST("/login", controller.Login)
+	r.POST("/pass", controller.ChangePassword)
+}
