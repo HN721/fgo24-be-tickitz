9964194ebea92663af27b0a6cb32d272a4fa3486
HN721 <hosea1422@gmail.com>
2025-07-02 11:56:24 +0700

    feat: call combinerouter on main go

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"weeklytickits/routers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+	routers.CombineRouter(r)
+	r.Run(fmt.Sprintf(":%s", os.Getenv("APP_PORT")))
+}
