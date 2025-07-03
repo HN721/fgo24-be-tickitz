92ffd2f0198c8d1ec287bcd53d692cf5aaf2360f
HN721 <hosea1422@gmail.com>
2025-07-03 11:06:55 +0700

    feat: create Controller Update Movies

--- models/movie.go
+++ models/movie.go
@@ -10,14 +10,14 @@ import (
 )
 
 type Movies struct {
-	Id          string    `db:"id"`
-	Title       string    `db:"title"`
-	Synopsis    string    `db:"synopsis"`
-	Background  string    `db:"background"`
-	Poster      string    `db:"poster"`
-	ReleaseDate time.Time `db:"release_date"`
-	Duration    int       `db:"duration"`
-	Price       int       `db:"price"`
+	Id          int       `json:"id,omitempty" db:"id"`
+	Title       string    `json:"title" db:"title"`
+	Synopsis    string    `json:"synopsis" db:"synopsis"`
+	Background  string    ` json:"background" db:"background"`
+	Poster      string    ` json:"poster" db:"poster"`
+	ReleaseDate time.Time ` json:"releaseDate" db:"release_date"`
+	Duration    int       `json:"duration" db:"duration"`
+	Price       int       `json:"price" db:"price"`
 }
 
 func GetAllMovies() ([]Movies, error) {
@@ -25,7 +25,7 @@ func GetAllMovies() ([]Movies, error) {
 	if err != nil {
 
 	}
-	query := `SELECT title, synopsis, background, poster, release_date, duration, price FROM movies`
+	query := `SELECT id,title, synopsis, background, poster, release_date, duration, price FROM movies`
 	result, err := conn.Query(context.Background(), query)
 	data, err := pgx.CollectRows[Movies](result, pgx.RowToStructByName)
 	if err != nil {
@@ -42,46 +42,46 @@ func InsertMovies(movie Movies) error {
 	if movie.Title == "" {
 		return fmt.Errorf("Judul Tidak Boleh Kosong")
 	}
-	query := `INSERT INTO movies(title, synopsis, background, poster, release_date, duration, price)VALUES($1,$2,$3,$4,$5,$6,$7)`
-	_, err = conn.Exec(context.Background(), query, movie.Title, movie.Synopsis, movie.Background, movie.Poster, movie.ReleaseDate, movie.Duration, movie.Price)
 	defer func() {
 		conn.Conn().Close(context.Background())
 	}()
 	return err
 }
-func UpdateMovies(movie Movies) error {
+func UpdateMovies(movie Movies, movieId int) error {
 	conn, err := utils.DBConnect()
 	if err != nil {
 		return err
 	}
-	var data bool
-	err = conn.QueryRow(context.Background(), `SELECT * FROM movies WHERE id = $1`, movie.Id).Scan(&data)
-	if err != nil {
-		return err
-	}
-	if !data {
-		return fmt.Errorf("movie with title %s not found", movie.Title)
-	}
+	defer func() {
+		conn.Conn().Close(context.Background())
+	}()
+
 	query := `
 	UPDATE movies 
-	SET synopsis = $1, background = $2, poster = $3, release_date = $4, duration = $5, price = $6
-	WHERE id = $7`
+	SET title = $1, synopsis = $2, background = $3, poster = $4, release_date = $5, duration = $6, price = $7
+	WHERE id = $8`
 
-	_, err = conn.Exec(context.Background(), query,
+	result, err := conn.Exec(context.Background(), query,
+		movie.Title,
 		movie.Synopsis,
 		movie.Background,
 		movie.Poster,
 		movie.ReleaseDate,
 		movie.Duration,
 		movie.Price,
-		movie.Id,
+		movieId,
 	)
-	defer func() {
-		conn.Conn().Close(context.Background())
-	}()
-	return err
+	if err != nil {
+		return err
+	}
 
+	if result.RowsAffected() == 0 {
+		return fmt.Errorf("movie with id %d not found", movieId)
+	}
+
+	return nil
 }
+
 func DeleteMovies(movieId int) error {
 	conn, err := utils.DBConnect()
 	if err != nil {
