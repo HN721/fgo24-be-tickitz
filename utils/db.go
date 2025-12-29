f7cee92d9359b84ae630d243fd1d9c9bf9d114fd
HN721 <hosea1422@gmail.com>
2025-12-29 11:48:18 +0700

    feat:change sprint

--- utils/db.go
+++ utils/db.go
@@ -3,28 +3,49 @@ package utils
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 )
 
-func DBConnect() (*pgxpool.Conn, error) {
-	godotenv.Load()
-	connection := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-		os.Getenv("PGUSER"),
-		os.Getenv("PGPASSWORD"),
-		os.Getenv("PGHOST"),
-		os.Getenv("PGPORT"),
-		os.Getenv("PGDATABASE"),
-	)
-	pool, err := pgxpool.New(
-		context.Background(), connection,
-	)
-	conn, err := pool.Acquire(context.Background())
-
-	if err != nil {
-		return conn, err
+func DBConnect() (*pgxpool.Pool, error) {
+	host := os.Getenv("PGHOST")
+	if host == "" {
+		host = "db" // docker service name
 	}
-	return conn, err
+
+	port := os.Getenv("PGPORT")
+	if port == "" {
+		port = "5432"
+	}
+
+	user := os.Getenv("PGUSER")
+	if user == "" {
+		user = "postgres"
+	}
+
+	password := os.Getenv("PGPASSWORD")
+	dbname := os.Getenv("PGDATABASE")
+	if dbname == "" {
+		dbname = "postgres"
+	}
+
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		user, password, host, port, dbname,
+	)
+
+	pool, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// test koneksi
+	if err := pool.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	log.Println("✅ Connected to PostgreSQL:", host)
+	return pool, nil
 }
